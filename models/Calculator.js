050b2c2a635cc4274ea1cd1f8d06ac3006a3c828
ztimofeev <ztimofeev01@gmail.com>
2017-05-08 21:56:00 +0300

    first commit of the project

---
+++ models/Calculator.js
@@ -0,0 +1,27 @@
+function Calculator(leftOperand, rightOperand, operator) {
+    this.leftOperand = leftOperand;
+    this.rightOperand = rightOperand;
+    this.operator = operator;
+    
+    this.calculateResult = function () {
+        let result = 0;
+
+        switch (this.operator){
+            case '+':
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case '-':
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case '*':
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case '/':
+                result = this.leftOperand / this.rightOperand;
+        }
+
+        return result;
+    }
+}
+
+module.exports = Calculator;
